aed22cea5d1884adeced3ea62c069ceb65738428
unknown <remotephone@gmail.com>
2017-02-27 21:13:52 -0600

    urlscan v1 done

--- ch2/misc test/test.py
+++ ch2/misc test/test.py
@@ -1,13 +1,7 @@
 import socket
 
-def main():
-	sites = open('sitestoresolve.txt','r')
-	for site in sites.readlines():
-		siteURL = site.strip('\n')
-			if siteURL == String:
-			siteURL.socket.gethostbyname()
-		print "[+] Site "+siteURL+" resolves to "
-		return
-
-if __name__ == "__main__":
-        main()
+sites = open('sitestoresolve.txt','r')
+for site in sites.readlines():
+	siteURL = site.strip('\n')
+	s = socket.gethostbyname(siteURL)
+	print "[+] Site "+siteURL+" resolves to "+s
